0e69368a999267b8250f2c4949f08a8276a5fbc9
wakhid15 <95697443+wakhid15@users.noreply.github.com>
2024-01-16 16:25:41 +0700

    Jawaban 2

--- resources/views/v_ampu.blade.php
+++ resources/views/v_ampu.blade.php
@@ -4,8 +4,40 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    
+    <style>
+        table {
+            border-collapse: collapse;
+            width: 100%;
+        }
+    
+        th, td {
+            padding: 10px; /* Adjust the padding as needed */
+            border: 1px solid #ddd; /* Add border for better visibility */
+        }
+    </style>
 </head>
 <body>
     Ini Halaman Mata Kuliah di Ampu
+    <h1>MATA KULIAH YANG DIAMPU</h1>
+
+    
+    <table>
+        <thead>
+            <tr>
+                <th>Nama</th>
+                <th>Pengampu Matakuliah</th>
+                <th>SKS</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>DOSEN</td>
+                <td>MATKUL</td>
+                <td>3</td>
+            </tr>
+            <!-- Add more <tr> elements for additional rows if needed -->
+        </tbody>
+    </table>
 </body>
 </html>
--- resources/views/v_data.blade.php
+++ resources/views/v_data.blade.php
@@ -7,5 +7,37 @@
 </head>
 <body>
     Ini Halaman Data
+    <h1>MATA KULIAH YANG DIAMPU</h1>
+
+    <style>
+        table {
+            border-collapse: collapse;
+            width: 100%;
+        }
+    
+        th, td {
+            padding: 10px; /* Adjust the padding as needed */
+            border: 1px solid #ddd; /* Add border for better visibility */
+        }
+    </style>
+    
+    <table>
+        <thead>
+            <tr>
+                <th>Nama</th>
+                <th>Pengampu Matakuliah</th>
+                <th>SKS</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>DOSEN</td>
+                <td></td>
+                <td>3</td>
+            </tr>
+            <!-- Add more <tr> elements for additional rows if needed -->
+        </tbody>
+    </table>
+    
 </body>
 </html>
